cdab33d3d1656b4c70c0e4e8bb9c7b5e93609500
Mortens <mortens.4444@gmail.com>
2026-06-07 23:40:59 +0200

    Mtf.Lego.Mindstorms.EV3 NuGet package release 2.0.4

--- Mtf.Lego.Mindstorms.EV3/Enums/EnumLikeObject.cs
+++ Mtf.Lego.Mindstorms.EV3/Enums/EnumLikeObject.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
+using System.Runtime.CompilerServices;
 
 namespace Mtf.Lego.Mindstorms.EV3.Enums;
 
 public abstract class EnumLikeObject<T>(byte value, string name)
 {
+    private static bool initialized;
+
     public static readonly Dictionary<byte, T> Values = [];
 
     public byte Value { get; private set; } = value;
@@ -17,11 +20,13 @@ public abstract class EnumLikeObject<T>(byte value, string name)
 
     public static IList GetValues()
     {
+        EnsureInitialized();
         return Values.Values.ToList();
     }
 
     public static IList GetNotCombinedValues()
     {
+        EnsureInitialized();
         var powersOfTwo = new List<byte>();
         for (int i = 0; i < 8; i++)
         {
@@ -32,8 +37,19 @@ public abstract class EnumLikeObject<T>(byte value, string name)
 
     public static T? Parse(string name)
     {
+        EnsureInitialized();
         var fields = typeof(T).GetFields();
         var fieldInfo = fields.Single(field => String.Equals(field.Name, name, StringComparison.OrdinalIgnoreCase));
         return (T?)fieldInfo.GetValue(null);
+    }
+
+    private static void EnsureInitialized()
+    {
+        if (!initialized)
+        {
+            initialized = true;
+            var type = typeof(T);
+            RuntimeHelpers.RunClassConstructor(type.TypeHandle);
+        }
     }
 }
